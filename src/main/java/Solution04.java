b7212cb7a5fb6932240dacb0aabc88b4f9013ae8
Fareed <fareedally1999@gmail.com>
2021-09-05 15:35:16 -0400

    Initial commit

---
+++ src/main/java/Solution04.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution04
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter a noun: ");
+        String noun = in.nextLine();
+
+        System.out.println("Enter a verb: ");
+        String verb = in.nextLine();
+
+        System.out.println("Enter an adjective: ");
+        String adjective = in.nextLine();
+
+        System.out.println("Enter an adverb: ");
+        String adverb = in.nextLine();
+
+        System.out.println("Do you " + verb + " your " + adjective + " " +  noun + " " + adverb + "? That's hilarious!");
+    }
+}
